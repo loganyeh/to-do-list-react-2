48fa2551ff161bc3fc01e4aba61becc118892985
loganyeh <yehlogan@gmail.com>
2025-12-01 13:52:08 -0500

    fix: handleComplete checks of every task

--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -2,27 +2,26 @@ import { useContext } from "react";
 import { MyContext } from "./MyContext";
 
 function Task({id, description}) {
-    const {setTasks, taskCount, setTaskCount, lineThrough, setLineThrough} = useContext(MyContext);
+    const {tasks, setTasks, taskCount, setTaskCount, isComplete, setIsComplete} = useContext(MyContext);
 
     const handleDelete = (itemToDelete) => {
         setTasks(prevItems => prevItems.filter(item => item.description !== itemToDelete));
     }
 
-    const handleLineThrough = (id) => {
-        setLineThrough(prev => id ? `line-through` : "");
-        
+    const handleComplete = () => {
+        setIsComplete(prev => !prev);
+        console.log(`isComplete is ${isComplete} at ${id}`);
     }
 
     return (
         <>
              <div className="h-24 w-1/2 flex flew-row mt-5">
-                <div onClick={() => handleLineThrough(id)} className={`h-full w-2/3 flex justify-center items-center border-2 border-red-600 hover:bg-gray-50 active:bg-gray-200 cursor-pointer 
-                    ${lineThrough ? `line-through` : ""}
-                `}>
+                <div onClick={handleComplete} id={id} className={`h-full w-2/3 flex justify-center items-center border-2 border-red-600 hover:bg-gray-50 active:bg-gray-200 cursor-pointer`}>
                     {description}<br/>
+                    {isComplete ? `✅ ` : ``}
                     id: {id}
                 </div>
-                <button onClick={() => handleDelete(description)} id={taskCount} className="h-full w-1/3 font-semibold text-black text-xl border-2 border-l-0 border-red-600 hover:bg-gray-50 active:bg-gray-200">Delete</button>
+                <button onClick={() => handleDelete(description)} className="h-full w-1/3 font-semibold text-black text-xl border-2 border-l-0 border-red-600 hover:bg-gray-50 active:bg-gray-200">Delete</button>
             </div>
         </>
     )
--- src/component/ToDoList.jsx
+++ src/component/ToDoList.jsx
@@ -6,12 +6,12 @@ function ToDoList() {
     // const {} = useContext(MyContext);
     const [taskCount, setTaskCount] = useState(3);
     const [tasks, setTasks] = useState([
-        {id: 0, description: "Walk the dog"},
-        {id: 1, description: "Take out the trash"},
-        {id: 2, description: "Wash the dishes"},
+        {id: 0, description: "Walk the dog", isComplete: false},
+        {id: 1, description: "Take out the trash", isComplete: false},
+        {id: 2, description: "Wash the dishes", isComplete: false},
     ]);
     const [inputValue, setInputValue] = useState('');
-    const [lineThrough, setLineThrough] = useState(false);
+    const [isComplete, setIsComplete] = useState(false);
 
     const handleInputValue = (event) => {
         setInputValue(event.target.value);
@@ -30,14 +30,14 @@ function ToDoList() {
         } 
         else{
             setInputValue('');
-            setTasks(task => [...task, {id: taskCount, description: inputValue}]);
+            setTasks(task => [...task, {id: taskCount, description: inputValue, isComplete}]);
             setTaskCount(prev => prev + 1);
         }
     };
 
     return (
         <>
-            <MyContext.Provider value={{setTasks, taskCount, setTaskCount, lineThrough, setLineThrough}}>
+            <MyContext.Provider value={{setTasks, taskCount, setTaskCount, isComplete, setIsComplete}}>
                 <div className="h-screen w-screen flex justify-center items-center border-2 border-red-600">
                     <div id="list-container" className="h-full w-1/2 flex flex-col items-center border-2 border-red-600">
                         {/* TITLE */}
@@ -53,7 +53,7 @@ function ToDoList() {
 
                             {/* TASKS */}
                             {tasks.map((task) => {
-                                return <Task key={task.id} id={task.id} description={task.description}/>
+                                return <Task key={task.id} id={task.id} description={task.description} isComplete={task.isComplete}/>
                             })}
 
                         </div>
