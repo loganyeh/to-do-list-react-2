c78c5ddd46a6b276cabcb732b21ae9547229ca0c
loganyeh <yehlogan@gmail.com>
2025-12-01 13:59:26 -0500

    fix: handleComplete

--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -16,7 +16,7 @@ function Task({id, description}) {
     return (
         <>
              <div className="h-24 w-1/2 flex flew-row mt-5">
-                <div onClick={handleComplete} id={id} className={`h-full w-2/3 flex justify-center items-center border-2 border-red-600 hover:bg-gray-50 active:bg-gray-200 cursor-pointer`}>
+                <div onClick={() => handleComplete()} id={id} className={`h-full w-2/3 flex justify-center items-center border-2 border-red-600 hover:bg-gray-50 active:bg-gray-200 cursor-pointer`}>
                     {description}<br/>
                     {isComplete ? `✅ ` : ``}
                     id: {id}
